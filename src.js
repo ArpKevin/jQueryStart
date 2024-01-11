bf65ff4bafe60dde1eb553ea7e2fa0df9468e678
Kevin <arpaskevin11@gmail.com>
2024-01-11 10:25:23 +0100

    jquery kezdes

---
+++ src.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // alert("a");
+
+    let $szoveg = $("h1").text();
+    console.log($szoveg);
+
+
+    $("h2").text("asd");
+
+    console.log($(".sarga").css("background-color"));
+
+    $(".sarga").css("background", "yellow");
+
+    $("body").css("background", "blue");
+
+    $("h1").css("color", "red");
+})
+
+// $(window).on("Load", function(){
+
+// })
